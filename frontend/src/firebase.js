f4164fed54351db38a5e2cab90fb2cba2d28250f
Rupam Sau <rupamsau0010@users.noreply.github.com>
2021-01-04 09:04:08 +0530

    firebase setting and file, cleanup react app

---
+++ frontend/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAxoVbxRS-Btc2pU7mk4JzHSpjDr79A9aM",
+  authDomain: "conversewith-rupumsau0010.firebaseapp.com",
+  projectId: "conversewith-rupumsau0010",
+  storageBucket: "conversewith-rupumsau0010.appspot.com",
+  messagingSenderId: "119634537246",
+  appId: "1:119634537246:web:c71747a19c3c5c3cf2770f",
+};
