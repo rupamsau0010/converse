2374ee22c030e1682e86bf34bd537521ca31d047
Rupam Sau <rupamsau0010@users.noreply.github.com>
2021-01-04 17:26:08 +0530

    chat body done. Looking for backend

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { Avatar, IconButton } from "@material-ui/core";
 import "./chat.css";
-import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import {
+  AttachFile,
+  InsertEmoticon,
+  Mic,
+  MoreVert,
+  SearchOutlined,
+  SendOutlined,
+} from "@material-ui/icons";
 
 const Chat = () => {
   return (
@@ -23,6 +30,35 @@ const Chat = () => {
             <MoreVert />
           </IconButton>
         </div>
+      </div>
+      <div className="chat_body">
+        <p className="chat_message">
+          <span className="chat_name">Rupam</span>
+          This is a message
+          <span className="chat_timestamp">{new Date().toUTCString()}</span>
+        </p>
+        <p className="chat_message chat_reciever">
+          <span className="chat_name">Rupam</span>
+          This is a message
+          <span className="chat_timestamp">{new Date().toUTCString()}</span>
+        </p>
+        <p className="chat_message">
+          <span className="chat_name">Rupam</span>
+          This is a message
+          <span className="chat_timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+      <div className="chat_footer">
+        <InsertEmoticon />
+        <form>
+          <input placeholder="Type a message" type="text"></input>
+          <button type="submit">
+            <IconButton>
+              <SendOutlined />
+            </IconButton>
+          </button>
+        </form>
+        <Mic />
       </div>
     </div>
   );
