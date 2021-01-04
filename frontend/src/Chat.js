113166397ac817cebf932647bc1bee5f59df8a44
Rupam Sau <rupamsau0010@users.noreply.github.com>
2021-01-04 09:19:29 +0530

    adding chat and sidebar.js into App.js

---
+++ frontend/src/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./chat.css"
+
+const Chat = () => {
+    return (
+        <div>
+            <h1> I am Chat</h1>
+        </div>
+    )
+}
+
+export default Chat
---
+++ frontend/src/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./sidebar.css"
+
+const sidebar = () => {
+    return (
+        <div className="sidebar">
+            <h1>I am sidebar</h1>
+        </div>
+    )
+}
+
+export default sidebar
