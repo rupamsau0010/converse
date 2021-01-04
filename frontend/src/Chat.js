15c8b6ab00374b426fe1d377450032c83d8a5d72
Rupam Sau <rupamsau0010@users.noreply.github.com>
2021-01-04 16:06:39 +0530

    chat header complete

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { Avatar, IconButton } from "@material-ui/core";
 import "./chat.css";
+import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
 
 const Chat = () => {
   return (
-    <div>
-      <h1> I am Chat</h1>
+    <div className="chat">
+      <div className="chat_header">
+        <Avatar />
+        <div className="chat_headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
+        </div>
+        <div className="chat_headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 };
