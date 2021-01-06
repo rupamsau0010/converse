7795ed1ebc55269edfc8a0145c548c3706e26f6a
Rupam Sau <rupamsau0010@users.noreply.github.com>
2021-01-06 10:52:14 +0530

    uri changed

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios"
 
+const SERVER_URI = ""
 if(process.env.NODE_ENV === "production") {
     SERVER_URI = "http://localhost:5000"
 } else {
-    SERVER_URI = 'HEROKU_URI'
+    SERVER_URI = process.env.HEROKU_URI
 }
 
 const instance = axios.create({
