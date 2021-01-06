e3183952363559b6d5a32963bdfe4ef9d5d20367
Rupam Sau <rupamsau0010@users.noreply.github.com>
2021-01-06 10:42:40 +0530

    heroku uri change

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,14 @@
 import axios from "axios"
 
+if(process.env.NODE_ENV === "production") {
+    SERVER_URI = "http://localhost:5000"
+} else {
+    SERVER_URI = 'HEROKU_URI'
+}
+
 const instance = axios.create({
-    baseURL: "http://localhost:5000"
+    baseURL: SERVER_URI
 })
+console.log(process.env.NODE_ENV);
 
 export default instance
